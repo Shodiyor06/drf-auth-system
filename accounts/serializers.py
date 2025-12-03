2676b89319d2ed72dabdc464090763d57d9f51a2
djumanov <djumanovdev@gmail.com>
2025-12-03 17:55:05 +0500

    register

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,6 +4,13 @@ from rest_framework import serializers
 User = get_user_model()
 
 
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
 class RegisterSerializer(serializers.ModelSerializer):
     confirm = serializers.CharField(max_length=128)
 
@@ -16,3 +23,14 @@ class RegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('password and confirm are nor the same value.')
         
         return super().validate(attrs)
+
+    def create(self, validated_data):
+        validated_data.pop('confirm')
+        password = validated_data.pop('password')
+
+        user = User(**validated_data)   
+        user.set_password(password)
+        user.save()
+
+        return user
+    
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserSerializer
 
 
 class RegisterView(APIView):
@@ -11,6 +11,10 @@ class RegisterView(APIView):
         serializer = RegisterSerializer(data=request.data)
 
         if serializer.is_valid(raise_exception=True):
-            return Response(serializer.validated_data)
+            user = serializer.save()
+
+            user_json = UserSerializer(user).data
+
+            return Response(user_json, status=status.HTTP_201_CREATED)
         
         return Response(status=status.HTTP_400_BAD_REQUEST)
